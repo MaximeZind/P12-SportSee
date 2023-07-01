b17a19d6695d47672fa93aa6a486d919284cd415
Maxime Zinderstein <maximezinderstein@gmail.com>
2023-07-01 20:59:19 +0200

    le radar est en blanc au lieu de gris

--- P12-Sportsee/src/components/Performances.jsx
+++ P12-Sportsee/src/components/Performances.jsx
@@ -22,7 +22,7 @@ function Performances({userId}) {
         <div className={classes.performances_chart}>
             <ResponsiveContainer width='100%' height='100%'>
                 <RadarChart outerRadius='70%' data={newArray} style={{ backgroundColor: '#282D30' }} >
-                <PolarAngleAxis dataKey="kind" angleAxisId={0} tick={{ fontSize: 10, color: '#FFF' }}/>
+                <PolarAngleAxis dataKey="kind" angleAxisId={0} tick={{ fontSize: 10 }} stroke='#FFF'/>
                     <PolarGrid />
                     <Radar name="Performances" dataKey="value" fill="#FF0101" fillOpacity={0.6} />
                 </RadarChart>
