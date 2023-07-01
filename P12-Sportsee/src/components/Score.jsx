b8e8711839b85c6e04b75f8fb14bda67ddf7258a
Maxime Zinderstein <maximezinderstein@gmail.com>
2023-07-01 21:35:10 +0200

    Le composant a une couleur de fond

--- P12-Sportsee/src/components/Score.jsx
+++ P12-Sportsee/src/components/Score.jsx
@@ -43,7 +43,7 @@ function Score({userId}) {
                 <p>de votre objectif</p>
             </div>
             <ResponsiveContainer className={classes.piechart} width='100%' height='100%'>
-                <PieChart backgroundColor='#FBFBFB'>
+                <PieChart style={{backgroundColor: '#FBFBFB'}}>
                     <Pie data={chartData} dataKey="value" nameKey="name" cx="50%" cy="50%" innerRadius={70} outerRadius={80} fill="#FF0000" startAngle={220} endAngle={-140} />
                     <Pie data={innerCircle} dataKey="value" nameKey="name" cx="50%" cy="50%" innerRadius={0} outerRadius={70} fill="#FFFFFF" />
                 </PieChart>
